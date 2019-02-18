e226e65b280e45322124150596b7a0d53cc2d6df
Slav1Bak <players7@interia.pl>
2019-02-18 15:47:53 +0100

    init

---
+++ src/Carview.java
@@ -0,0 +1,45 @@
+import controller.Carcontroller;
+
+import java.util.Scanner;
+
+public class Carview {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Auto factory");
+        boolean flag = true;
+        //utworzenie obiebktu kontrolera
+        Carcontroller cc = new Carcontroller();
+        while (flag) {
+            System.out.println("1. Zamowienie auta \n2. Lista zamowien \n0. Wyjscie");
+            //odczyt wartosci ze skanera\
+            int decision = scan.nextInt();
+            //konsumpcja
+            scan.nextLine();
+
+            switch (decision) {
+                case 1:
+                    System.out.println("Podaj marke");
+                    String brand = scan.nextLine();
+                    System.out.println("Podaj model");
+                    String model = scan.nextLine();
+                    System.out.println("podaj cene");
+                    double price = scan.nextDouble();
+                    System.out.println("podaj rok");
+                    int year = scan.nextInt();
+                    //wczytanie brand, model, price, year ze skanerra
+                    cc.purchaseCar(brand, model, price, year);
+                    break;
+                case 2:
+                    System.out.println(Carcontroller.getIndex());
+                    cc.getCars();
+                    break;
+                case 0:
+                    flag = false;
+                    break;
+                default:
+                    System.out.println("Błedny wybor frajerze");
+            }
+
+        }
+    }
+}
--- src/controller/Company.java
+++ src/controller/Company.java
@@ -19,13 +19,15 @@ public class Company {
                 pesel,
                 birth_date,
                 empl_date,
-               salary_net);
+                salary_net);
 
 
         //zapisanie pracownika do listy pracownikow
 
 
         employees[index] = e;
+        //inkrementacja indexu
+        index++;
 
         //wyswietlenie danych zapisaego pracownika
 
@@ -33,10 +35,43 @@ public class Company {
 
     }
 
+    public void getAllEmployess() {
+        System.out.println("lista pracownikow");
+        for (int i = 0; i < index; i++) {
+            System.out.println(employees[i]);
+        }
+
+    }
+
+    public Employee getEmployeeByPesel(String Pesel_search) {
+        for (int i = 0; i < index; i++) {
+            if (employees[i].Pesel.equals(Pesel_search)) {
+                return employees[i];
+            }
+        }
+        return null;
+    }
+
+    public void setSalary(String Pesel_search, double newSalary) {
+
+        for (int i = 0; i < index; i++) {
+            if (employees[i].Pesel.equals(Pesel_search)) {
+                employees[i].salary_net = newSalary;
+            }
+        }
+    }
     public static void main(String[] args) {
         Company c = new Company();
-        c.addEmployee("Adam","Kowalski","92090910201","1992-09-09","2017-11-12",2100);
-        c.addEmployee("Albert","Kowalewski","92090910201","1992-09-09","2017-11-12",2400);
-        c.addEmployee("Zenobiusz","Kowalkiewicz","92090910201","1992-09-09","2017-11-12",20100);
+        //wywolanie metody
+        c.addEmployee("Adam", "Kowalski", "92090910201", "1992-09-09", "2017-11-12", 2100);
+        c.addEmployee("Albert", "Kowalewski", "92090910221", "1992-09-09", "2017-11-12", 2400);
+        c.addEmployee("Zenobiusz", "Kowalkiewicz", "92090910201", "1992-09-09", "2017-11-12", 20100);
+        c.getAllEmployess();
+        System.out.println("szukani po pesel");
+        System.out.println(c.getEmployeeByPesel("333"));
+        System.out.println(c.getEmployeeByPesel("92090910221"));
+        c.setSalary("92090910221", 10000);
+        System.out.println();
+        c.getAllEmployess();
     }
 }
